b51b5f0c93a0671530b88bb8483e439ef974f5d0
Robert Brodrecht <robertdot@gmail.com>
2014-03-29 22:50:35 -0500

    More grid-related classes

--- index.php
+++ index.php
@@ -11,11 +11,4 @@
 
 get_header();	
 launchpad_get_template_part('content', get_post_type());
-?>
-<div class="row no-gutter small-to-100 medium-to-gutter">
-	<div class="column-25 medium-to-50" style="background: #FCC">real: 1; push: 1</div>
-	<div class="column-25 medium-to-50 push-50" style="background: #CFC">real: 2; push: 3</div>
-	<div class="column-50 medium-to-100 pull-25" style="background: #CCF">real: 3; push: 2</div>
-</div>
-<?php
 get_footer();
