1fb452f4a9dbd5995bf854d1c899a062c2065dff
Robert Brodrecht <robertdot@gmail.com>
2014-03-31 23:36:33 -0500

    Short review to add direct child selector.

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "css/fonts"
 
-output_style = :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
