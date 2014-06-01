0e28dec75ff2cf0890cbcbda2b9fcba68f30c479
Robert Brodrecht <robertdot@gmail.com>
2014-06-01 14:38:00 -0500

    Starting file reorganization

--- functions.php
+++ functions.php
@@ -18,18 +18,18 @@ locate_template('functions-custom.php', true, true);
 
 
 /** System functions like theme activation, rewrites, etc. */
-locate_template('functions/system.php', true, true);
+locate_template('lib/core/system.php', true, true);
 /** Modifications to the admin area like options pages and admin cleanup. */
-locate_template('functions/admin.php', true, true);
+locate_template('lib/core/admin.php', true, true);
 /** Security related features like limit login attempts. */
-locate_template('functions/security.php', true, true);
+locate_template('lib/core/security.php', true, true);
 /** Post Type related code for registering and creating metaboxes.  */
-locate_template('functions/post-types.php', true, true);
+locate_template('lib/core/post-types.php', true, true);
 /** Code for custom API calls.  */
-locate_template('functions/api.php', true, true);
+locate_template('lib/core/api.php', true, true);
 /** Template related modifications such as nav menu registration, header cleanup, page cache, etc.  */
-locate_template('functions/template.php', true, true);
+locate_template('lib/core/template.php', true, true);
 /** Cache-related functions.  */
-locate_template('functions/cache.php', true, true);
+locate_template('lib/core/cache.php', true, true);
 /** Custom functions for handling various duties.  */
-locate_template('functions/utilities.php', true, true);
+locate_template('lib/core/utilities.php', true, true);
