cf3b5852c8a8bd7b01f28a1a1b2896a9e7e4295a
Robert Brodrecht <robertdot@gmail.com>
2015-02-17 15:31:10 -0600

    child theme modifications to get cruft out of header

--- lib/core/template.php
+++ lib/core/template.php
@@ -59,6 +59,164 @@ function launchpad_theme_setup() {
 	launchpad_image_setup();
 }
 add_action('after_setup_theme', 'launchpad_theme_setup');
+	
+
+/**
+ * Add Support for Custom CSS and JS for Critical Path
+ *
+ * @since		1.6
+ */
+function launchpad_add_head_modifications() {
+	global $site_options, $wp_query;
+	
+	$add_this_id = $site_options['add_this_id'];
+	
+	$ajax = '';
+	if(isset($site_options['ajax_page_loads']) && $site_options['ajax_page_loads'] === true) {
+		$ajax = 'true';
+	}
+	
+	$excerpt = launchpad_seo_excerpt();
+
+	?>
+	
+		<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		
+		<link rel="icon" href="/images/icons/favicon.png">
+		<link rel="icon" href="/images/icons/favicon_2x.png" media="(-webkit-min-device-pixel-ratio: 2)">
+		
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name') ?> RSS Feed" href="/feed/">
+		<?php if(is_single() || is_page()) { ?>
+		
+		<link rel="canonical" href="http://<?php echo $_SERVER['HTTP_HOST'] ?><?php the_permalink(); ?>">
+		<?php } ?>
+		
+		<link rel="apple-touch-icon" sizes="57x57"   href="/images/icons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="72x72"   href="/images/icons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/images/icons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/images/icons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/images/icons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/images/icons/apple-touch-icon-152x152.png">
+		
+		<link href="/images/icons/startup-iphone-320x460.jpg" rel="apple-touch-startup-image" media="(device-width: 320px)">
+		<link href="/images/icons/startup-iphone4-640x920.jpg" rel="apple-touch-startup-image" media="(device-width: 320px) and (-webkit-min-device-pixel-ratio: 2)">
+		<link href="/images/icons/startup-iphone5-640x1096.jpg" rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-min-device-pixel-ratio: 2)">
+		<link href="/images/icons/ipad-portrait-768x1004.jpg" rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait)">
+		<link href="/images/icons/ipad-landscape-1024x748.jpg" rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: landscape)">
+		<link href="/images/icons/ipad-retina-portrait-1536x2008.jpg" rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: portrait) and (-webkit-device-pixel-ratio: 2)">
+		<link href="/images/icons/ipad-retina-landscape-2048x1496.jpg" rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (orientation: landscape) and (-webkit-device-pixel-ratio: 2)">
+		
+		<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0">
+		<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name') ?>">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		
+		<meta name="description" content="<?php echo $excerpt; ?>">
+		<?php
+		
+		if(!get_option('blog_public')) {
+			echo '<meta name="robots" content="noindex, nofollow">';			
+		} else if(!$wp_query->is_single && !$wp_query->is_singular && !is_front_page()) {
+			echo '<meta name="robots" content="noindex, follow">';
+		}
+		
+		?>
+		<?php if(isset($site_options['fb_app_id']) && $site_options['fb_app_id']) { ?>
+		
+		<meta property="fb:app_id" content="<?php echo $site_options['fb_app_id'] ?>">
+		<?php } ?>
+		<?php if(isset($site_options['fb_admin_id']) && $site_options['fb_admin_id']) { ?>
+		<?php foreach(explode(',', $site_options['fb_admin_id']) as $fb_admin_id) { ?>
+		
+		<meta property="fb:admins" content="<?php echo trim($fb_admin_id) ?>">
+		<?php } ?>
+		<?php } ?>
+		
+		<?php
+		
+		$card_type = 'website';
+		if(is_single() || is_singular()) {
+			$card_type = 'article';
+		}
+		
+		?>
+		
+		<meta property="og:title" content="<?php launchpad_title(true); ?>">
+		<meta property="og:description" content="<?php echo $excerpt; ?>">
+		<meta property="og:type" content="<?php echo $card_type ?>">
+		<meta property="og:url" content="http://<?php echo $_SERVER['HTTP_HOST'] ?><?php the_permalink(); ?>">
+		<meta property="og:site_name" content="<?php bloginfo('name') ?>">
+		<?php
+		
+		if(has_post_thumbnail()) {
+			$thumbnail = get_post_thumbnail_id();
+			$thumbnail = wp_get_attachment_image_src($thumbnail, 'opengraph');
+			if($thumbnail) {
+				?>
+				
+		<meta property="og:image" content="http://<?php echo $_SERVER['HTTP_HOST'] ?><?php echo $thumbnail[0] ?>">
+		<meta property="og:image:width" content="<?php echo $thumbnail[1] ?>">
+		<meta property="og:image:height" content="<?php echo $thumbnail[2] ?>">
+				<?php
+			}
+		}
+		
+		?>
+	
+		<?php
+		
+		$card_type = 'summary';
+		if((is_single() || is_singular()) && has_post_thumbnail()) {
+			$card_type = 'summary_with_large_image';
+		}
+		
+		?>
+	
+		<meta property="twitter:card" content="<?php echo $card_type ?>">
+		<meta property="twitter:url" content="http://<?php echo $_SERVER['HTTP_HOST'] ?><?php the_permalink(); ?>">
+		<meta property="twitter:title" content="<?php launchpad_title(true); ?>">
+		<meta property="twitter:description" content="<?php echo $excerpt; ?>">
+		<?php
+		
+		if(has_post_thumbnail()) {
+			$thumbnail = get_post_thumbnail_id();
+			$thumbnail = wp_get_attachment_image_src($thumbnail, 'large'); // Large to hopefully stay under 1MB.
+			if($thumbnail) {
+				?>
+	
+		<meta property="twitter:image" content="http://<?php echo $_SERVER['HTTP_HOST'] ?><?php echo $thumbnail[0] ?>">
+		<meta property="twitter:image:width" content="<?php echo $thumbnail[1] ?>">
+		<meta property="twitter:image:height" content="<?php echo $thumbnail[2] ?>">
+				<?php
+			}
+		}
+		
+		?>
+		<?php if(isset($site_options['twitter_card_username']) && $site_options['twitter_card_username']) { ?>
+		
+		<meta property="twitter:site" content="@<?php echo $site_options['twitter_card_username'] ?>">
+		<?php } ?>
+	
+		<?php if(defined('GA_ID') && GA_ID != '') { ?>
+	
+		<script id="google-analytics">
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', '<?php echo GA_ID ?>', 'auto');
+		  ga('send', 'pageview');
+		</script>
+		<?php 
+		
+		}
+	
+		if(stristr($_SERVER['HTTP_HOST'], '.dev') !== false || stristr($_SERVER['HTTP_HOST'], '.git') !== false) {
+			echo "<script>window.dev = true;</script>\n";
+		}
+}
+add_action('wp_head', 'launchpad_add_head_modifications', 1);
 
 
 /**
@@ -71,6 +229,10 @@ function launchpad_enqueue_scripts() {
 	
 	$load_footer = get_option('active_plugins') ? false : true;
 	if(!is_admin()) {
+		
+		wp_enqueue_style('launchpad_screen', '/css/screen.css', array(), null, 'screen, projection, handheld, tv');
+		wp_enqueue_style('launchpad_print', '/css/print.css', array(), null, 'print');
+		
 		wp_deregister_script('jquery');
 		wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-1.11.1.min.js', false, null, $load_footer);
 		wp_enqueue_script('jquery');
